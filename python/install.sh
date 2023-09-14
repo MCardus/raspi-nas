196e4ecefa94903ed9436e72f2098fa81ddbbf3a
Marc Cardus <marc.cardusgarcia@gbgplc.com>
2023-09-14 22:25:08 +0200

    python fixed

--- python/install.sh
+++ python/install.sh
@@ -1,2 +1,2 @@
-sudo apt install python3-pip python3-venv -y
+sudo apt install python3-pip python3-venv libncurses-dev libgdbm-dev libz-dev tk-dev libsqlite3-dev libreadline-dev liblzma-dev libffi-dev libssl-dev -y
 curl https://pyenv.run | bash
