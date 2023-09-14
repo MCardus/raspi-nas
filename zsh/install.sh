fbe50ef3ee51c720fade40200f485ab109fcae0a
Marc Cardus <marc.cardusgarcia@gbgplc.com>
2023-09-14 19:11:46 +0200

    fix zsh plugin

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,2 +1,5 @@
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
