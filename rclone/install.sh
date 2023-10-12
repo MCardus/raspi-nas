b13fbc2489c877bad8ae2a0c9da12bc6befe8d44
Marc Cardus <marc.cardusgarcia@gbgplc.com>
2023-10-12 21:25:03 +0200

    add timer for rclone

--- rclone/install.sh
+++ rclone/install.sh
@@ -1,3 +1,5 @@
 sudo apt-get -y install rclone
 cat systemcmd.txt > /etc/systemd/system/rclone@$(whoami).service
+cat systemcmd_timer.txt > /etc/systemd/system/rclone@$(whoami).timer
+sudo systemctl enable rclone@$(whoami).timer
 rclone config
