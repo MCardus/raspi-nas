a7f8e147a7e24eb82bd4e3d90b8695643b763150
Marc Cardus <marc.cardusgarcia@gbgplc.com>
2023-12-28 15:29:30 +0100

    couple fixes in transmission

--- transmission/install.sh
+++ transmission/install.sh
@@ -1,5 +1,6 @@
 # https://pimylifeup.com/raspberry-pi-transmission/
 sudo systemctl stop transmission-daemon
-sudo mkdir /downloads
-sudo chown -R debian-transmission:debian-transmission /downloads
+sudo mkdir -P /mnt/toshiba/downloads
+sudo chown -R debian-transmission:debian-transmission /mnt/toshiba/downloads
 cp transmission/settings.json /etc/transmission-daemon/settings.json
+sudo systemctl start transmission-daemon
